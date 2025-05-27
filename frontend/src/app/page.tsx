a893c7fc59022c4a153ec85472976fcc2ffa7f92
swarui <stevewarui88@gmail.com>
2025-05-27 22:30:04 +0300

    strigometry

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,20 @@
 import Navbar from '@/components/Navbar'
+import Image from "next/image";
 
 
 function Page(){
   return (<>
   <Navbar/>
   <h1>Mtaaaa</h1>
+  <div className="relative w-full h-[700px]">
+  <Image
+    src="/images/home.webp"
+    alt="Hero"
+    fill
+    className="object-cover"
+  />
+</div>
+
   </>)
 }
 export default Page
