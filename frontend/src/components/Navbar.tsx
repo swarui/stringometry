222ed29d694fe7692b48c7513718fec9a1947d67
swarui <stevewarui88@gmail.com>
2025-05-27 20:00:28 +0300

    strigometry

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,14 +3,14 @@ import { ShoppingCart, Search } from "lucide-react";
 
 function Navbar() {
   return (
-    <nav className="w-full flex items-center justify-between px-6 py-4 shadow-md bg-white">
+    <nav className="w-full flex items-center justify-between px-6 py-4 shadow-md h-20 bg-white">
       {/* Left: Logo */}
       <div className="flex items-center gap-2">
         <Image src="/images/logo.avif" alt="Logo" width={250} height={250} />
       </div>
 
       {/*Links */}
-      <div className="flex text-black items-center gap-8 text-lg font-medium">
+      <div  className="flex text-black items-center gap-8 text-lg font-medium font-futura">
         <a href="#" className="hover:text-blue-600">
           Home
         </a>
