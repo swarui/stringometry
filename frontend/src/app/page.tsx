2f31987500cc5f9ac43cf8913d6fd15b040ed3a4
swarui <stevewarui88@gmail.com>
2025-05-27 19:49:05 +0300

    strigometry

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from '@/components/Navbar'
+
+
+function Page(){
+  return (<>
+  <Navbar/>
+  <h1>Mtaaaa</h1>
+  </>)
+}
+export default Page
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import { ShoppingCart, Search } from "lucide-react";
+
+function Navbar() {
+  return (
+    <nav className="w-full flex items-center justify-between px-6 py-4 shadow-md bg-white">
+      {/* Left: Logo */}
+      <div className="flex items-center gap-2">
+        <Image src="/images/logo.avif" alt="Logo" width={250} height={250} />
+      </div>
+
+      {/*Links */}
+      <div className="flex text-black items-center gap-8 text-lg font-medium">
+        <a href="#" className="hover:text-blue-600">
+          Home
+        </a>
+        <a href="#" className="hover:text-blue-600">
+          For Sale
+        </a>
+        <a href="#" className="hover:text-blue-600">
+          Art
+        </a>
+        <a href="#" className="hover:text-blue-600">
+          Contact
+        </a>
+      </div>
+
+      {/*Icons */}
+      <div className="flex  text-black items-center gap-4">
+        <Search className="w-5 h-5 cursor-pointer hover:text-blue-600" />
+        <ShoppingCart className="w-5 h-5 cursor-pointer hover:text-blue-600" />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
